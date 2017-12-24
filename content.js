ce52dde5814329402bf7d3804936512742117137
Norskovsen <martin_n@me.com>
2017-12-24 12:49:51 +0100

    Implemented all the functionality

--- content.js
+++ content.js
@@ -1,7 +1,43 @@
 chrome.runtime.onMessage.addListener(receiver);
+console.log('Content script loaded');
 
 function receiver(request, sender, sendResponse) {
   if (request.message === "user clicked!") {
-      alert('test');
+      loadComments();
+      console.log('Button clicked');
   }
+}
+
+function loadComments(){
+    let loadCommentsLink = document.body.querySelector('a.UFIPagerLink');
+    if(loadCommentsLink){
+	loadCommentsLink.click();
+	setTimeout(loadComments, 100);
+    }else{
+	chooseComment()
+    }
+}
+
+function chooseComment(){
+    removePrevious();
+    let comments = document.querySelector('div._3b-9._j6a div').children;
+    let chosenNum = random(comments.length);
+    let prevComment = comments[chosenNum-1];
+    let chosenComment = comments[chosenNum];
+    chosenComment.id = "chosenComment";
+    chosenComment.style.backgroundColor = "gray";
+    prevComment.scrollIntoView();
+}
+
+function removePrevious(){
+    let preChosenComment = document.getElementById('chosenComment');
+    console.log(preChosenComment);
+    if(preChosenComment){
+	preChosenComment.style.backgroundColor = "";
+	preChosenComment.id = "";
+    }
+}
+
+function random(num){
+    return Math.floor(Math.random()*num);
 }
